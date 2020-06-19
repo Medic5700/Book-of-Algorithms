07f3c9fc2b54ca007a333c4c1af7b3070bf11930
Medic5700 <super_alias_the_first@hotmail.com>
2020-06-19 12:02:52 -0400

    Minimum viable product

--- dumbALU.py
+++ dumbALU.py
@@ -7,10 +7,9 @@ class DumbALU:
         self.memory['r'] = [0 for i in range(registerSize)] #standard registers
         self.memory['m'] = [0 for i in range(memorySize)] #standard memory
         self.memory['s'] = [0] #special register for storing operation literals
-        print(self.memory)
+        self.memory['pc'] = 0
         self.code = None
         self.lastState = None
-        self.pc = 0
 
         self._snapshot()
 
@@ -20,22 +19,47 @@ class DumbALU:
         lastState['r'] = [self.memory['r'][j] for j in range(len(self.memory['r']))]
         lastState['m'] = [self.memory['m'][j] for j in range(len(self.memory['m']))]
         lastState['s'] = [self.memory['s'][0]]
+        lastState['pc'] = self.memory['pc']
         self.lastState = lastState
     
     def _display(self, operation, readList, writeList):
-        """displays the current operation"""
+        """displays the current operation
 
-        text = ""
-        line1 = str(operation) + "\t\t" + "[" + str(bin(self.memory['s'][0])[2:].rjust(self.bitLength, '0')) + "]" + "\n"
+        does not scale, will need to be replaced with 'verticle orentation'"""
+        readColour = "\u001b[96m" #forground teal
+        writeColour = "\u001b[31m" #forground red
+        ansiEnd = "\u001b[0m"
+
+        line1 = '0x' + hex(self.lastState['pc'])[2:].rjust(2, '0') + '    '
+        line1 += str(operation.expandtabs(8))
+        line1 = line1.ljust(80 - (self.bitLength+2), ' ')
+
+        if 's0' in readList:
+            line1 += "[" + readColour + str(bin(self.memory['s'][0])[2:].rjust(self.bitLength, '0')) + ansiEnd + "]"
+        else:
+            line1 += ''.ljust(self.bitLength+2, ' ')
+        line1 += "\n"
         
-        line2 = "  "
+        line2 = "   r"
         for i in range(len(self.lastState['r'])):
-            line2 += "[" + str(bin(self.lastState['r'][i])[2:].rjust(self.bitLength, '0')) + "]"
+            line2 += "["
+            if ('r'+str(i)) in readList:
+                line2 += readColour
+            line2 += str(bin(self.lastState['r'][i])[2:].rjust(self.bitLength, '0'))
+            if ('r'+str(i)) in readList:
+                line2 += ansiEnd
+            line2 += ']'
         line2 += "\n"
 
-        line3 = "  "
+        line3 = "   r"
         for i in range(len(self.memory['r'])):
-            line3 += "[" + str(bin(self.memory['r'][i])[2:].rjust(self.bitLength, '0')) + "]"
+            line3 += "["
+            if ('r'+str(i)) in writeList:
+                line3 += writeColour
+            line3 += str(bin(self.memory['r'][i])[2:].rjust(self.bitLength, '0'))
+            if ('r'+str(i)) in writeList:
+                line3 += ansiEnd
+            line3 += ']'
         line3 += ""
 
         print(line1 + line2 + line3)
@@ -49,10 +73,12 @@ class DumbALU:
         pass
 
     def opNoop(self, lineNumber, text="no-op"):
-        self._snapshot()
         self._display(text, [], [])
 
     def opLoad(self, lineNumber, operation, source, destination):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        self.memory['pc'] += 1
         operation = "load" + "\t" + str(source) + " -> " + str(destination)
 
         if type(source) is int:
@@ -67,7 +93,10 @@ class DumbALU:
                       )
 
     def opAnd (self, lineNumber, operation, source1, source2, destination):
+        self.memory['pc'] = lineNumber
         self._snapshot()
+        self.memory['pc'] += 1
+        operation = "and" + "\t" + str(source1) + ', ' + str(source2) + " -> " + str(destination)
 
         if type(source1) is int:
             self.memory['s'] = [source1]
@@ -79,16 +108,134 @@ class DumbALU:
 
         self.memory[destination[0]][int(destination[1:])] = self.memory[source1[0]][int(source1[1:])] & self.memory[source2[0]][int(source2[1:])]
 
-        self._display("and",
+        self._display(operation,
                       [source1, source2],
                       [destination]
                       )
 
-    def opShiftL(self):
-        pass
+    def opShiftL(self, lineNumber, operation, source1, source2, destination):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        self.memory['pc'] += 1
+        operation = "shiftL" + "\t" + str(source1) + ', ' + str(source2) + " -> " + str(destination)
 
-    def opAdd(self):
-        pass
+        if type(source1) is int:
+            self.memory['s'] = [source1]
+            source1 = 's0'
+
+        if type(source2) is int:
+            self.memory['s'] = [source2]
+            source2 = 's0'
+
+        self.memory[destination[0]][int(destination[1:])] = self.memory[source1[0]][int(source1[1:])] << self.memory[source2[0]][int(source2[1:])]
+
+        self._display(operation,
+                      [source1, source2],
+                      [destination]
+                      )
+        
+    def opShiftR(self, lineNumber, operation, source1, source2, destination):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        self.memory['pc'] += 1
+        operation = "shiftR" + "\t" + str(source1) + ', ' + str(source2) + " -> " + str(destination)
+
+        if type(source1) is int:
+            self.memory['s'] = [source1]
+            source1 = 's0'
+
+        if type(source2) is int:
+            self.memory['s'] = [source2]
+            source2 = 's0'
+
+        self.memory[destination[0]][int(destination[1:])] = self.memory[source1[0]][int(source1[1:])] >> self.memory[source2[0]][int(source2[1:])]
+
+        self._display(operation,
+                      [source1, source2],
+                      [destination]
+                      )
+
+    def opAdd(self, lineNumber, operation, source1, source2, destination):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        self.memory['pc'] += 1
+        operation = "add" + "\t" + str(source1) + ', ' + str(source2) + " -> " + str(destination)
+
+        if type(source1) is int:
+            self.memory['s'] = [source1]
+            source1 = 's0'
+
+        if type(source2) is int:
+            self.memory['s'] = [source2]
+            source2 = 's0'
+
+        self.memory[destination[0]][int(destination[1:])] = self.memory[source1[0]][int(source1[1:])] + self.memory[source2[0]][int(source2[1:])]
+
+        self._display(operation,
+                      [source1, source2],
+                      [destination]
+                      )
+
+    def opJump(self, lineNumber, operation, pc):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        operation = "jump" + "\t" + str(pc) + " -> " + "pc"
+
+        if type(pc) is int:
+            self.memory['s'] = [pc]
+            pc = 's0'
+        self.memory['pc'] = lineNumber
+
+        self._display(operation,
+                      [],
+                      []
+                      )
+
+    def braEqual(self, lineNumber, operation, source1, source2, pc):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        operation = '==' + '\t' + str(source1) + ', ' + str(source2) + ' -> ' + str(pc)
+        
+        if type(source1) is int:
+            self.memory['s'] = [source1]
+            source1 = 's0'
+
+        if type(source2) is int:
+            self.memory['s'] = [source2]
+            source2 = 's0'
+
+        if self.memory[source1[0]][int(source1[1:])] == self.memory[source2[0]][int(source2[1:])]:
+            self.memory['pc'] = pc
+        else:
+            self.memory['pc'] += 1
+            
+        self._display(operation,
+                      [],
+                      []
+                      )
+
+    def braNotEqual(self, lineNumber, operation, source1, source2, pc):
+        self.memory['pc'] = lineNumber
+        self._snapshot()
+        operation = '!=' + '\t' + str(source1) + ', ' + str(source2) + ' -> ' + str(pc)
+        
+        if type(source1) is int:
+            self.memory['s'] = [source1]
+            source1 = 's0'
+
+        if type(source2) is int:
+            self.memory['s'] = [source2]
+            source2 = 's0'
+
+        if self.memory[source1[0]][int(source1[1:])] != self.memory[source2[0]][int(source2[1:])]:
+            self.memory['pc'] = pc
+        else:
+            self.memory['pc'] += 1
+            
+        self._display(operation,
+                      [],
+                      []
+                      )
 
 def multiply1(a, b, bitlength=8):
     '''Takes in two unsigned integers, a, b -> returns an integer a*b
@@ -107,25 +254,29 @@ def multiply1(a, b, bitlength=8):
 
     ALU.opLoad(0, "load", a, 'r0')
     r[0] = a #needs to be double the bit size of the number inputs
-    ALU.opLoad(0, "load", b, 'r1')
+    ALU.opLoad(1, "load", b, 'r1')
     r[1] = b
-    ALU.opLoad(0, "load", 0, 'r3')
+    ALU.opLoad(2, "load", 0, 'r3')
     r[3] = 0 #needs to be double the bit size of the number inputs
 
     #visualize(r, bitlength, "no-op", True)
 
-    for i in range(bitlength):
-        ALU.opAnd(0, 'and', 'r1', 1, 'r2')
+    ALU.braEqual(3, '==', 'r1', 0, 11)
+    while(r[1] != 0):
+    #for i in range(bitlength):
+        ALU.opAnd(4, 'and', 'r1', 1, 'r2')
         r[2] = r[1] & 1
-        #visualize(r, bitlength, "and", False, rh=[1], sh=[2])
         #visualize(r, bitlength, "compare", False, rh=[2])
+        ALU.braNotEqual(5, '!=', 'r2', 1, 8)
         if r[2] == 1:
+            ALU.opAdd(6, 'add', 'r0', 'r3', 'r3')
             r[3] = r[0] + r[3]
-            #visualize(r, bitlength, "add", False, rh=[0,3], sh=[3])
+        ALU.opShiftL(8, 'shiftL', 'r0', 1, 'r0')
         r[0] = r[0] << 1
-        #visualize(r, bitlength, "shift", False, rh=[0], sh=[0])
+        ALU.opShiftR(9, 'shiftR', 'r1', 1, 'r1')
         r[1] = r[1] >> 1
-        #visualize(r, bitlength, "shift", True, rh=[1], sh=[1])
+        ALU.opJump(10, 'jump', 3)
+    ALU.opNoop('eof')
 
     z = r[3]
     
@@ -133,4 +284,4 @@ def multiply1(a, b, bitlength=8):
     return z
 
 if __name__ == "__main__":
-    multiply1(2,5)
+    print(multiply1(3,4))
