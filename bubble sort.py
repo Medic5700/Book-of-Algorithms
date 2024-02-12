68e673f05027f722fdc7b5ecf0c13a9c1f5717ce
Medic5700 <Medic5700@users.noreply.github.com>
2024-02-12 09:40:26 -0500

    various improvements to 'bubble sort.py'

--- bubble sort.py
+++ bubble sort.py
@@ -20,7 +20,7 @@ def visualize(A : list[int], newline : bool = True, dataComparisons : int = 0, d
     assert type(A) is list
     assert all([type(i) is int for i in A])
     assert len(A) <= 64
-    assert all([True if (0 <= i <= 255) else False for i in A])
+    assert all([(0 <= i <= 255) for i in A])
 
     assert type(newline) is bool
 
@@ -33,11 +33,11 @@ def visualize(A : list[int], newline : bool = True, dataComparisons : int = 0, d
     assert type(highlight) is list
     assert (len(highlight) == 0) or (len(highlight) == 2)
     assert all([type(i) is int for i in highlight])
-    assert all([True if (0 <= i <= len(A)) else False for i in highlight]), highlight
+    assert all([(0 <= i <= len(A)) for i in highlight]), highlight
     
     global animationDelay
 
-    colourScaling : int = 256 // len(A)
+    colourScaling : int = 255 // len(A)
     B : list[int] = copy.deepcopy(A)
     B.sort()
 
@@ -59,7 +59,10 @@ def visualize(A : list[int], newline : bool = True, dataComparisons : int = 0, d
         endline = "\n"
     else:
         endline = ""
-    print("\u001b[0G" + "[" + ''.join(line) + "\u001b[0m" + "]" + f"    DataComparisons = {str(dataComparisons).rjust(4, '0')};" + f"    DataSwaps = {str(dataSwaps).rjust(4, '0')}", end=endline, flush=True)
+    print("\u001b[0G" + "[" + ''.join(line) + "\u001b[0m" + "]" + \
+            f"    DataComparisons = {str(dataComparisons).rjust(4, '0')};" + \
+            f"    DataSwaps = {str(dataSwaps).rjust(4, '0')}", \
+            end=endline, flush=True)
     time.sleep(animationDelay)
 
 def _isSorted(A : list[int]) -> bool:
@@ -83,51 +86,55 @@ def bubbleSort1(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
     
-    for _ in range(len(A)):
+    for _ in range(len(B)):
         j : int
-        for j in range(len(A) - 1):
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+        for j in range(len(B) - 1):
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
-        visualize(A, True, dataComparisons, dataSwaps)
+                dataSwaps += 1                                                  #DEMO
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 def bubbleSort2(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers"""
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
 
     i : int
-    for i in range(len(A)):
+    for i in range(len(B)):
         j : int
-        for j in range(len(A) - 1 - i):
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+        for j in range(len(B) - 1 - i):
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
-        visualize(A, True, dataComparisons, dataSwaps)
+                dataSwaps += 1                                                  #DEMO
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 def bubbleSort3(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers
@@ -138,31 +145,33 @@ def bubbleSort3(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
 
     i : int
-    for i in range(len(A)):
+    for i in range(len(B)):
         swapped : bool = False
         j : int
-        for j in range(len(A) - 1 - i):
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
+        for j in range(len(B) - 1 - i):
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
                 swapped = True
 
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
-        visualize(A, True, dataComparisons, dataSwaps)
-        if swapped == False:
+                dataSwaps += 1                                                  #DEMO
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
+        if not swapped:
             break
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)
+    return B
 
 def cocktailShakerSort1(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers
@@ -173,36 +182,38 @@ def cocktailShakerSort1(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
 
     i : int
-    for i in range(len(A)//2):
+    for i in range(len(B) // 2):
         j : int
-        for j in range(i, len(A) - 1 - i, 1): # goes forward through array
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+        for j in range(i, len(B) - 1 - i, 1): # goes forward through array
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
+                dataSwaps += 1                                                  #DEMO
         j : int
-        for j in range(len(A) - 2 - i, i -1, -1): # goes backwards through array
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+        for j in range(len(B) - 2 - i, i -1, -1): # goes backwards through array
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
-        visualize(A, True, dataComparisons, dataSwaps)
+                dataSwaps += 1                                                  #DEMO
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 def cocktailShakerSort2(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers
@@ -213,43 +224,45 @@ def cocktailShakerSort2(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
 
     i : int
-    for i in range(len(A)//2):
+    for i in range(len(B) // 2):
         swapped : bool = False
         j : int
-        for j in range(i, len(A) - 1 - i, 1): # goes forward through array
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
+        for j in range(i, len(B) - 1 - i, 1): # goes forward through array
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
                 swapped = True
 
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
+                dataSwaps += 1                                                  #DEMO
         j : int
-        for j in range(len(A) - 2 - i, i -1, -1): # goes backwards through array
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [j, j+1])
-            if A[j] > A[j+1]:
+        for j in range(len(B) - 2 - i, i - 1, -1): # goes backwards through array
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [j, j+1])           #DEMO
+            if B[j] > B[j+1]:
                 swapped = True
 
-                temp : int = A[j+1]
-                A[j+1] = A[j]
-                A[j] = temp
+                temp : int = B[j+1]
+                B[j+1] = B[j]
+                B[j] = temp
 
-                dataSwaps += 1
-        visualize(A, True, dataComparisons, dataSwaps)
-        if swapped == False:
+                dataSwaps += 1                                                  #DEMO
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
+        if not swapped:
             break
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 def gnomeSort1(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers
@@ -260,31 +273,33 @@ def gnomeSort1(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
+
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
 
     i : int = 0
-    while i < (len(A)): # fails when at end of list, and thus everything is sorted
+    while i < (len(B)): # fails when at end of list, and thus everything is sorted
         if (i == 0): # when at beginning of list, move right
-            visualize(A, False, dataComparisons, dataSwaps, [i, i])
+            visualize(B, False, dataComparisons, dataSwaps, [i, i])             #DEMO
             i += 1
-        elif (A[i] >= A[i-1]): # when both elements are storted, move right
-            dataComparisons += 1
-            visualize(A, False, dataComparisons, dataSwaps, [i-1, i])
+        elif (B[i] >= B[i-1]): # when both elements are storted, move right
+            dataComparisons += 1                                                #DEMO
+            visualize(B, False, dataComparisons, dataSwaps, [i-1, i])           #DEMO
             i += 1
         else: # both elements aren't sorted, swap and move left
-            visualize(A, False, dataComparisons, dataSwaps, [i-1, i])
-            dataSwaps += 1
+            visualize(B, False, dataComparisons, dataSwaps, [i-1, i])           #DEMO
+            dataSwaps += 1                                                      #DEMO
 
-            temp : int = A[i]
-            A[i] = A[i-1]
-            A[i-1] = temp
+            temp : int = B[i]
+            B[i] = B[i-1]
+            B[i-1] = temp
             
             i += -1
     
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 def combSort1(A : list[int]) -> list[int]:
     """Takes an unsorted array of integers, A -> returns an array of sorted integers
@@ -295,39 +310,41 @@ def combSort1(A : list[int]) -> list[int]:
     assert type(A) is list
     assert all([type(i) is int for i in A])
 
-    dataComparisons : int = 0
-    dataSwaps : int = 0
-    visualize(A, True, dataComparisons, dataSwaps)
+    B : list[int] = copy.deepcopy(A)
 
-    gap : int = len(A)
+    dataComparisons : int = 0                                                   #DEMO
+    dataSwaps : int = 0                                                         #DEMO
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+
+    gap : int = len(B)
     shrink : float = 1.3
     isSorted : bool = False
 
     while not isSorted:
-        visualize(A, False, dataComparisons, dataSwaps)
+        visualize(B, False, dataComparisons, dataSwaps)                         #DEMO
 
         gap = int(gap // shrink)
         if gap <= 1:
             gap = 1
             isSorted = True
 
-        for i in range(len(A) - gap):
-            visualize(A, False, dataComparisons, dataSwaps, [i, i+gap])
-            dataComparisons += 1
+        for i in range(len(B) - gap):
+            visualize(B, False, dataComparisons, dataSwaps, [i, i+gap])         #DEMO
+            dataComparisons += 1                                                #DEMO
 
-            if A[i] > A[i+gap]:
-                temp : int = A[i]
-                A[i] = A[i+gap]
-                A[i+gap] = temp
+            if B[i] > B[i+gap]:
+                temp : int = B[i]
+                B[i] = B[i+gap]
+                B[i+gap] = temp
 
                 isSorted = False
 
-                dataSwaps += 1
+                dataSwaps += 1                                                  #DEMO
 
-        visualize(A, True, dataComparisons, dataSwaps)
+        visualize(B, True, dataComparisons, dataSwaps)                          #DEMO
 
-    visualize(A, True, dataComparisons, dataSwaps)
-    return A
+    visualize(B, True, dataComparisons, dataSwaps)                              #DEMO
+    return B
 
 if __name__ == "__main__":
     A : list[int] = [None for _ in range(16)]
@@ -340,7 +357,7 @@ if __name__ == "__main__":
                 A[i] = x
                 break
     # A is unsorted list, will not contain duplicats for demonstration purposes
-    print("Generating unsorted list ".ljust(80, '-'))
+    print("Generated unsorted list ".ljust(80, '-'))
     print(A)
 
     print("showing BubbleSort1 ".ljust(80, '-'))
